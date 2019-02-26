fe187b99cd5c5cb797483918ef1a675e0e90981d
Elle Mouton <elle.mouton@gmail.com>
2019-02-26 19:17:32 +0200

    reads in raw file into stream

--- manipulator.cpp
+++ manipulator.cpp
@@ -36,8 +36,8 @@ void MTNELL004::VolImage::rowExtract(int sliceId){
 	std::cout << "Row Extract\n";
 }
 
+// populate the object with images in stack and set member variables
 bool MTNELL004::VolImage::readImages(std::string baseName){
-	// populate the object with images in stack and set member variables
 
 	//get file name by appending ".dat" to the base
 	std::stringstream file;
@@ -49,18 +49,57 @@ bool MTNELL004::VolImage::readImages(std::string baseName){
 	myfile.open(file_name);
 	if(!myfile){
 		std::cerr << "Header file not found\n";
+		myfile.close();
+		return false;
 	}
-	else{
-		int width, height, num_images;
-		myfile >> width >> std::ws >> height >> std::ws >> num_images >> std::ws;
-	}
+
+	//read in dimensions and use them to set class variables
+	int w, h, num_images;
+	myfile >> w >> std::ws >> h >> std::ws >> num_images >> std::ws;
+	width = w;
+	height = h;
+
+	//close header file
 	myfile.close();
 
-	std::cout << "Number of images: \nNumber of bytes required:\n";
+	//use base name to find images and num_images to iterate through all the images and build the image slices
+	for(int i=0; i<num_images; i++){
+
+		//get the .raw file name using the base name
+		std::stringstream file;
+		file << baseName <<i<< ".raw";
+		std::string raw_file = file.str();
+	
+		std::ifstream myfile;
+		myfile.open (raw_file,  std::ios::in | std::ios::binary);
+		if(!myfile){
+			std::cout << raw_file << " not found\n";
+			return false;
+		}
+
+
+		// get length of file:
+	    myfile.seekg (0, myfile.end);
+	    int length = myfile.tellg();
+	    myfile.seekg (0, myfile.beg);
+
+	    char* img = new char[length];
+
+	    myfile.read (img,length);
+
+	    
+
+	    delete[] img;
+
+		myfile.close();
+
+	}
+
+	
 
 	return true;
 }
 
 int MTNELL004::VolImage::volImageSize(void){
-	return 1;
+	return width;
 } 
--- volimg.cpp
+++ volimg.cpp
@@ -12,38 +12,43 @@ int main(int argc, char * argv[]){
 		//create new VolImage instance
 		MTNELL004::VolImage images;
 
-		//determine which option the user is choosing
-		int i;
-		int j;
-		std::string file_name;
+		//read in images
+		bool successRead = images.readImages(base);
 
-		switch(argc){
-			case 2:
-				//no arguments provided
-				break;
+		//if the read was successful, continue
+		if(successRead){
+			//determine which option the user is choosing
+			int i;
+			int j;
+			std::string file_name;
 
-			case 6:
-				i = MTNELL004::convertToInt(std::string(argv[3]));
-				j = MTNELL004::convertToInt(std::string(argv[4]));
-				file_name = std::string(argv[5]);
-				images.diffmap(i,j,file_name);
-				break;
+			switch(argc){
+				case 2:
+					//no arguments provided
+					break;
 
-			case 5:
-				i = MTNELL004::convertToInt(std::string(argv[3]));
-				file_name = std::string(argv[4]);
-				images.extract(i, file_name);
-				break;
+				case 6:
+					i = MTNELL004::convertToInt(std::string(argv[3]));
+					j = MTNELL004::convertToInt(std::string(argv[4]));
+					file_name = std::string(argv[5]);
+					images.diffmap(i,j,file_name);
+					break;
 
-			case 4:
-				i = MTNELL004::convertToInt(std::string(argv[3]));
-				images.rowExtract(i);
-				break;
+				case 5:
+					i = MTNELL004::convertToInt(std::string(argv[3]));
+					file_name = std::string(argv[4]);
+					images.extract(i, file_name);
+					break;
 
-			default:
-				std::cout << "invalid number of arguments\n";
+				case 4:
+					i = MTNELL004::convertToInt(std::string(argv[3]));
+					images.rowExtract(i);
+					break;
+
+				default:
+					std::cout << "invalid number of arguments\n";
+			}
 		}
-
 
 	}
 	else{
